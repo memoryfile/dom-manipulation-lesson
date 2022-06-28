372adb8beb4bfeaca85d458bd762b84c7ead1808
memoryfile-desktop <yuiotp999@protonmail.com>
2022-06-28 17:54:39 -0400

    Add redText const in JS

---
+++ main.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('#container');
+
+const redText = document.createElement('red-text');
+redText.style.color = 'red';
+redText.setAttribute('style');
+
+redText.textContent = 'Hey I\'m red!'
+
