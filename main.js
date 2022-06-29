872b488b1867534d6caa3644a7b226e858b2f9fd
memoryfile-desktop <yuiotp999@protonmail.com>
2022-06-29 13:55:19 -0400

    Create redText and p consts

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-const container = document.querySelector('#container');
+const redContainer = document.querySelector('#redContainer');
 
-const redText = document.createElement('red-text');
+const redText = document.createElement('div');
+const p = document.createElement("p");
+p.innerText= ("Hey I'm red!");
+redText.classList.add('redText');
+
 redText.style.color = 'red';
-redText.setAttribute('style');
+redText.setAttribute('style', 'color:red;');
 
-redText.textContent = 'Hey I\'m red!'
-
+redContainer.append(p);
