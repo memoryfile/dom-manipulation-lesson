455656671b06ef23736d418c23d34e11f08e92a9
memoryfile-desktop <yuiotp999@protonmail.com>
2022-06-29 13:55:35 -0400

    Delete redText const

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ const redContainer = document.querySelector('#redContainer');
 const redText = document.createElement('div');
 const p = document.createElement("p");
 p.innerText= ("Hey I'm red!");
-redText.classList.add('redText');
 
-redText.style.color = 'red';
-redText.setAttribute('style', 'color:red;');
+p.style.color = 'red';
+p.setAttribute('style', 'color:red;');
 
 redContainer.append(p);
