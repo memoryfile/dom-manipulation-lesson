f890527623928e438f16bae43ed8f18a87c32369
memoryfile-desktop <yuiotp999@protonmail.com>
2022-06-29 14:04:45 -0400

    Create blueh3

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-const redContainer = document.querySelector('#redContainer');
+const redContainer = document.querySelector("#redContainer");
 
-const redText = document.createElement('div');
+const redText = document.createElement("div");
 const p = document.createElement("p");
-p.innerText= ("Hey I'm red!");
-
-p.style.color = 'red';
-p.setAttribute('style', 'color:red;');
+p.innerText = "Hey I'm red!";
+p.style.color = "red";
+p.setAttribute("style", "color:red;");
 
 redContainer.append(p);
+
+const blueh3 = document.createElement("h3");
+blueh3.innerText = "I'm a blue h3!";
+blueh3.style.color = "blue";
+blueh3.setAttribute("style", "color:blue;");
+redContainer.append(blueh3);
